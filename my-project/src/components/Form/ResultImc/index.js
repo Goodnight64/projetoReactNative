31d2bdcc7e75a31400eaf16115dc684f0668cd8e
ARTHUR GOMES FELIX <90287093+Goodnight64@users.noreply.github.com>
2023-04-28 14:57:44 -0400

    add botao de compartilhamento

--- my-project/src/components/Form/ResultImc/index.js
+++ my-project/src/components/Form/ResultImc/index.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text,TouchableOpacity,Share } from "react-native";
 import styles from "./style";
 
 export default function ResultImc(props){
+
+    const onShare = async () =>{
+        const result = await Share.share({
+            message:"Meu imc e: " + props.ResultImc,
+        })
+    }
+
     return(
         <View style={styles.ResultImc}>
+            <View style={styles.boxSharebutton}>
+                {props.ResultImc != null ?
+                <TouchableOpacity 
+                onPress={() => onShare()}
+                style={styles.shared}>
+                    <Text style={styles.sharedText}>Share</Text>
+                </TouchableOpacity>
+                :
+                <View></View>
+                }
+            </View>
             <Text style={styles.information}>{props.messageResultImc}</Text>
             <Text style={styles.numberImc}>{props.ResultImc}</Text>
         </View>
